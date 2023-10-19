41f30cf3ea228575e2bf04872ff2f9ae2220db48
eleanorewu <yuenwu850823@gmail.com>
2023-10-19 10:21:22 +0800

    add meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  "app": {
+    "head": {
+      "viewport": "width=500, initial-scale=1",
+      "title": "Nuxt3 高效入門全攻略",
+      "meta": [
+          { "name": "description", "content": "這是 Mike 的 Nuxt3 高效入門全攻略課程" },
+          { "property": "og:title", "content": "Nuxt3 高效入門全攻略" },
+          { "property": "og:url", "content": "http://localhost:3000/" },
+          { "property": "og:description", "content": "這是 Mike 的 Nuxt3 高效入門全攻略課程" },
+      ]
+    }
+  }, 
   "imports": {
     "dirs": ['stores']
   },
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>首頁:</h1>
     <button>add</button>
+    <NuxtLink to="/about">關於我們</NuxtLink>
     <Card />
   </div>
 </template>
